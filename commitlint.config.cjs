abceb48ddee14c0094f38d9d34ae16a83ed84401
ukorvl <ukorotovskiy@gmail.com>
2026-03-05 15:28:13 +0800

    ci: add base ci structure and reusable actions

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -1,8 +1,9 @@
 module.exports = {
   extends: ["@commitlint/config-conventional"],
   rules: {
-    "type-empty": [2, "never"],
+    "header-max-length": [2, "always", 100],
     "subject-empty": [2, "never"],
+    "type-empty": [2, "never"],
     "type-enum": [
       2,
       "always",
@@ -20,6 +21,5 @@ module.exports = {
         "test",
       ],
     ],
-    "header-max-length": [2, "always", 100],
   },
 };
