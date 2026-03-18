5544bd2d7532e48d75c720599ead721967dc1009
OSHALKE <o.shalkevich@gmail.com>
2026-03-18 09:12:50 +0300

    ssh test 2

--- mobile/src/screens/App.tsx
+++ mobile/src/screens/App.tsx
@@ -6,9 +6,7 @@ export default function App() {
     <SafeAreaProvider>
       <SafeAreaView style={styles.container}>
         <View style={styles.content}>
-          <Text style={styles.title}>
-            Welcome to Unimetrics Mobile!!!
-          </Text>
+          <Text style={styles.title}>Welcome to Unimetrics Mobile with SSH!!!</Text>
         </View>
       </SafeAreaView>
     </SafeAreaProvider>
@@ -18,6 +16,11 @@ export default function App() {
 // TODO: remove after re-enabling NativeWind (restore className props)
 const styles = StyleSheet.create({
   container: { backgroundColor: "#6b7280", flex: 1 }, // bg-gray-500
-  content: { alignItems: "center", flex: 1, justifyContent: "center", paddingHorizontal: 24 },
+  content: {
+    alignItems: "center",
+    flex: 1,
+    justifyContent: "center",
+    paddingHorizontal: 24,
+  },
   title: { color: "#000", fontSize: 24, fontWeight: "bold" },
 });
